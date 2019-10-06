62cec6cd454b672408777a316a651a85bddea1d0
michael guatambu davis <guatambu@dundak.com>
2019-10-05 23:56:18 -0700

    adds Weeks 2-9 Data struct templates ready for filling out

---
+++ mindfulness_UCLA/Models/Week2Data.swift
@@ -0,0 +1,34 @@
+//
+//  Week2Data.swift
+//  mindfulness_UCLA
+//
+//  Created by Michael Guatambu Davis on 10/5/19.
+//  Copyright © 2019 DunDak, LLC. All rights reserved.
+//
+
+import Foundation
+
+struct Week2Data {
+    
+    //    static let week2 = WeekCourseContent(introTextParagraph1: <#T##(String, String)#>,
+    //                                         introTextParagraph2: <#T##(String, String)?#>,
+    //                                         introTextParagraph3: <#T##(String, String)?#>,
+    //                                         videosTextParagraph1: <#T##(String, String)#>,
+    //                                         videosTextParagraph2: <#T##(String, String)?#>,
+    //                                         videosTextParagraph3: <#T##(String, String)?#>,
+    //                                         readingsTextParagraph1: <#T##(String, String)#>,
+    //                                         readingsTextParagraph2: <#T##(String, String)?#>,
+    //                                         readingsTextParagraph3: <#T##(String, String)?#>,
+    //                                         dailyPracticesTextParagraph1: <#T##(String, String)#>,
+    //                                         dailyPracticesTextParagraph2: <#T##(String, String)?#>,
+    //                                         dailyPracticesTextParagraph3: <#T##(String, String)?#>,
+    //                                         miscellaneousTextParagraph1: <#T##(String, String)#>,
+    //                                         miscellaneousTextParagraph2: <#T##(String, String)?#>,
+    //                                         miscellaneousTextParagraph3: <#T##(String, String)?#>,
+    //                                         weekVideoURLList: <#T##[(String, String)]#>,
+    //                                         weekReadingURLList: <#T##[(String, String)]#>,
+    //                                         supplementalMaterialsURLList: <#T##[(String, String)]#>,
+    //                                         practiceSheetsURLList: <#T##[(String, String)]#>,
+    //                                         guidedMeditationPlaceholder: <#T##[(String, String)]#>)
+    
+}
---
+++ mindfulness_UCLA/Models/Week3Data.swift
@@ -0,0 +1,35 @@
+//
+//  Week3Data.swift
+//  mindfulness_UCLA
+//
+//  Created by Michael Guatambu Davis on 10/5/19.
+//  Copyright © 2019 DunDak, LLC. All rights reserved.
+//
+
+import Foundation
+
+struct Week3Data {
+    
+    //    static let week3 = WeekCourseContent(introTextParagraph1: <#T##(String, String)#>,
+    //                                         introTextParagraph2: <#T##(String, String)?#>,
+    //                                         introTextParagraph3: <#T##(String, String)?#>,
+    //                                         videosTextParagraph1: <#T##(String, String)#>,
+    //                                         videosTextParagraph2: <#T##(String, String)?#>,
+    //                                         videosTextParagraph3: <#T##(String, String)?#>,
+    //                                         readingsTextParagraph1: <#T##(String, String)#>,
+    //                                         readingsTextParagraph2: <#T##(String, String)?#>,
+    //                                         readingsTextParagraph3: <#T##(String, String)?#>,
+    //                                         dailyPracticesTextParagraph1: <#T##(String, String)#>,
+    //                                         dailyPracticesTextParagraph2: <#T##(String, String)?#>,
+    //                                         dailyPracticesTextParagraph3: <#T##(String, String)?#>,
+    //                                         miscellaneousTextParagraph1: <#T##(String, String)#>,
+    //                                         miscellaneousTextParagraph2: <#T##(String, String)?#>,
+    //                                         miscellaneousTextParagraph3: <#T##(String, String)?#>,
+    //                                         weekVideoURLList: <#T##[(String, String)]#>,
+    //                                         weekReadingURLList: <#T##[(String, String)]#>,
+    //                                         supplementalMaterialsURLList: <#T##[(String, String)]#>,
+    //                                         practiceSheetsURLList: <#T##[(String, String)]#>,
+    //                                         guidedMeditationPlaceholder: <#T##[(String, String)]#>)
+    
+}
+
