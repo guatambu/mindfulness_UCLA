8d702497d686c7b30716870f2d017f084e509810
michael guatambu davis <guatambu@dundak.com>
2019-10-06 22:21:06 -0700

    adds properties' content to Week3Data.swift

--- mindfulness_UCLA/Models/Week3Data.swift
+++ mindfulness_UCLA/Models/Week3Data.swift
@@ -10,26 +10,52 @@ import Foundation
 
 struct Week3Data {
     
-    //    static let week3 = WeekCourseContent(introTextParagraph1: <#T##(String, String)#>,
-    //                                         introTextParagraph2: <#T##(String, String)?#>,
-    //                                         introTextParagraph3: <#T##(String, String)?#>,
-    //                                         videosTextParagraph1: <#T##(String, String)#>,
-    //                                         videosTextParagraph2: <#T##(String, String)?#>,
-    //                                         videosTextParagraph3: <#T##(String, String)?#>,
-    //                                         readingsTextParagraph1: <#T##(String, String)#>,
-    //                                         readingsTextParagraph2: <#T##(String, String)?#>,
-    //                                         readingsTextParagraph3: <#T##(String, String)?#>,
-    //                                         dailyPracticesTextParagraph1: <#T##(String, String)#>,
-    //                                         dailyPracticesTextParagraph2: <#T##(String, String)?#>,
-    //                                         dailyPracticesTextParagraph3: <#T##(String, String)?#>,
-    //                                         miscellaneousTextParagraph1: <#T##(String, String)#>,
-    //                                         miscellaneousTextParagraph2: <#T##(String, String)?#>,
-    //                                         miscellaneousTextParagraph3: <#T##(String, String)?#>,
-    //                                         weekVideoURLList: <#T##[(String, String)]#>,
-    //                                         weekReadingURLList: <#T##[(String, String)]#>,
-    //                                         supplementalMaterialsURLList: <#T##[(String, String)]#>,
-    //                                         practiceSheetsURLList: <#T##[(String, String)]#>,
-    //                                         guidedMeditationPlaceholder: <#T##[(String, String)]#>)
-    
+    static let week3 = WeekCourseContent(
+        lessonTitle: "Week 3 - DEaling with Thoughts",
+        lessonSubTitle: "Introduction to Yoga",
+        introTextParagraph1: ("", "At this point in your practice, you may be noticing just how busy the mind is, with replays of past events, expectations/fears about the future, or evaluations of present experience (I like this, don't like that, when will this be over, etc.)."),
+        introTextParagraph2: ("", "It can seem that the object of meditation is to get rid of all thoughts and distractions, and when that isn't happening, you may feel like you are failing at meditation and/or the meditation can turn into a struggle, wanting your experience to be different than it is. In fact, the kind of meditation you are learning is more about working skillfully with *any* internal or external experience, and isn't really about \"getting rid\" of thoughts and distractions. In fact, it can be said that wandering thoughts are the weights that train the muscles of the mind."),
+        introTextParagraph3: nil,
+        videosTextParagraph1: ("videos", "It's common for there to be a sense of pushing oneself in meditation, and the first video, *Non-Striving*, addresses the possibility of taking the striving out of meditation (and life!). The second video, *Attention, Intention, Attitude*, explores the attitude we take toward ourselves in meditation, which is often harsh, and the possibility of being gentler with ourselves. In the next two videos, *Your Thoughts are Bubbles* and *Dealing with Thoughts (in life and meditation)*, Jon Kabat-Zinn and Tara Brach each talk about the idea that meditation is not about getting rid of thoughts, but about changing our relationship with them. Finally, *The Samurai and the Fly* is a vivid dramatization illustrating how trying to stop thoughts or push away distractions not only doesn't work, it actually makes things worse."),
+        videosTextParagraph2: nil,
+        videosTextParagraph3: nil,
+        readingsTextParagraph1: ("readings", "The reading list this week begins with *Mindful Yoga* by Jon Kabat-Zinn. #Even if you are an experienced yoga practitioner, it's important to read this before beginning the Yoga practice that begins this week#. In *Meditation: It's Not What You Think*, Jon Kabat-Zinn points out that while meditation can often be accompanied by peace and relaxation, #\"meditation is not relaxation spelled differently\"#, and as Erik Wikstrom says in the quote above, #\"The moments of stillness are one of meditation's byproducts, not the practice itself.\"# The last three brief articles, by Wes Nisker and Jack Kornfield, explore more deeply the relationship between meditation and thoughts."),
+        readingsTextParagraph2: nil,
+        readingsTextParagraph3: nil,
+        dailyPracticesTextParagraph1: ("daily practices", "For the *formal practice* this week, we introduce *Mindful Yoga* (Yoga 1) into your 30 minute practice, alternating with the Sitting Meditation and choosing one day to do a Body Scan.  This is all outlined in your Week 3 Practice Log, below.   #*Even if you are an experienced yoga practitioner, be sure to read \"Mindful Yoga\" in the reading below, before beginning the yoga practice. Mindful Yoga is different than many traditional yoga practices in that there is less of a focus on the exact posture achieved and more focus on body/mind awareness.*#"),
+        dailyPracticesTextParagraph2: ("", "The *informal practice*  is about becoming aware of how we experience and process unpleasant events.  Just as in previous weeks, allow a few minutes before going to sleep to complete the informal practices log. #*Below are your materials for this week:*#"),
+        dailyPracticesTextParagraph3: nil,
+        miscellaneousTextParagraph1: ("", ""),
+        miscellaneousTextParagraph2: nil,
+        miscellaneousTextParagraph3: nil,
+        weekVideoURLList: [
+            ("*Non-Striving* ~- Jon Kabat-Zinn (3 min)~", "https://www.youtube.com/watch?v=Ye3xwfECuYc&index=1&list=PLbiVpU59JkVZzL8TqgU6Uz_HxhnMqpzZv"),
+            ("*Attention, Intention, Attitude* ~- Shauna Shapiro (16 min)~", "https://www.youtube.com/watch?v=KsXNUpZnyg4&list=PLbiVpU59JkVZzL8TqgU6Uz_HxhnMqpzZv&index=2"),
+            ("*Your Thoughts are Bubbles* ~- Jon Kabat-Zinn (5 min)~", "https://www.youtube.com/watch?v=w8Nsa45d0XE&index=3&list=PLbiVpU59JkVZzL8TqgU6Uz_HxhnMqpzZv"),
+            ("*Dealing with Thoughts (in life and in meditation)* ~- Tara Brach (20 min)~", "https://www.youtube.com/watch?v=9kNXhJf6jOM&index=4&list=PLbiVpU59JkVZzL8TqgU6Uz_HxhnMqpzZv"),
+            ("*The Samurai and the Fly* ~- Hanjin Song (3 min)~", "https://www.youtube.com/watch?v=R5-HNXxc5kk&list=PLbiVpU59JkVZzL8TqgU6Uz_HxhnMqpzZv&index=5")
+        ],
+        weekReadingURLList: [
+            ("*Mindful Yoga* ~- Jon Kabat-Zinn [excerpted from Full Catastrophe Living]~", "https://palousemindfulness.com/docs/yoga.pdf"),
+            ("*Meditation - It's Not What You Think* ~- Jon Kabat-Zinn~", "https://palousemindfulness.com/docs/JKZ_thinking.pdf"),
+            ("*Hadn't Thought of That* ~- Wes Nisker~", "https://palousemindfulness.com/docs/nisker-thoughts.pdf"),
+            ("*Your Mind: Friend or Foe?* ~- Jack Kornfield~", "https://palousemindfulness.com/docs/your-mind-friend-or-foe.pdf"),
+            ("*The Reality Below Thoughts* ~- Jack Kornfield~", "https://palousemindfulness.com/docs/reality-below-thoughts.pdf")
+        ],
+        supplementalMaterialsURLList: [
+            ("*Thoughts: \"Real, but not True\"* ~video by Tara Brach (26 min)~", "https://www.youtube.com/watch?v=iCrCmqeWHp4"),
+            ("*Surrender to the Monkeys*  ~video by Tara Brach (4 min)~", "https://www.youtube.com/watch?v=H8yoWnEmTO8"),
+            ("*Freedom from Repetitive Thoughts* ~article by Jack Kornfield~", "https://jackkornfield.com/expand-field-attention/"),
+            ("*\"Letting Go\" in Meditation* ~video by Peter Russell and Shauna Shapiro (15 min)~", "https://www.youtube.com/watch?v=H812bBkoHNk"),
+            ("*Autobiography in Five Short Chapters* ~one-page \"autobiography\" by Portia Nelson~", "https://palousemindfulness.com/docs/autobio_5chapters.pdf"),
+            ("*Transform Your Mind, Change Your Brain* ~video by Richard Davidson [65 min]~", "http://www.youtube.com/watch?v=7tRdDqXgsJ0"),
+            ("*Mindfulness, the Mind, and Addictive Behavior* ~video by Judson Brewer [20 min]~", "https://www.youtube.com/watch?v=7a9sWI0vJzc")
+        ],
+        practiceSheetsURLList: [
+            ("\(WeekHeadings.formalPracticeSheet.rawValue) - Mindful Yoga (Yoga 1), Body Scan, Sitting Meditation", "TBD"),
+            ("\(WeekHeadings.informalPracticeSheet.rawValue) - Unpleasant Events Calendar", "TBD")
+        ],
+        guidedMeditationPlaceholder: [("Mindful Yoga 1 (37 min)", "https://www.youtube.com/watch?v=Teo0f1ab1PY&index=3&list=PLbiVpU59JkVaFMGi0A8Im_hfSh-SWsFwg")]
+    )
 }
 
