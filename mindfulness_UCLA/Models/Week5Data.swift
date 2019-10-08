7e7a25bb94c7610f863b09959802fbd6756e996b
michael guatambu davis <guatambu@dundak.com>
2019-10-07 18:32:20 -0700

    adds majorityh of Week5Data properties' contents to data model object, still a bt more to do...

--- mindfulness_UCLA/Models/Week5Data.swift
+++ mindfulness_UCLA/Models/Week5Data.swift
@@ -10,25 +10,69 @@ import Foundation
 
 struct Week5Data {
     
-    //    static let week5 = WeekCourseContent(introTextParagraph1: <#T##(String, String)#>,
-    //                                         introTextParagraph2: <#T##(String, String)?#>,
-    //                                         introTextParagraph3: <#T##(String, String)?#>,
-    //                                         videosTextParagraph1: <#T##(String, String)#>,
-    //                                         videosTextParagraph2: <#T##(String, String)?#>,
-    //                                         videosTextParagraph3: <#T##(String, String)?#>,
-    //                                         readingsTextParagraph1: <#T##(String, String)#>,
-    //                                         readingsTextParagraph2: <#T##(String, String)?#>,
-    //                                         readingsTextParagraph3: <#T##(String, String)?#>,
-    //                                         dailyPracticesTextParagraph1: <#T##(String, String)#>,
-    //                                         dailyPracticesTextParagraph2: <#T##(String, String)?#>,
-    //                                         dailyPracticesTextParagraph3: <#T##(String, String)?#>,
-    //                                         miscellaneousTextParagraph1: <#T##(String, String)#>,
-    //                                         miscellaneousTextParagraph2: <#T##(String, String)?#>,
-    //                                         miscellaneousTextParagraph3: <#T##(String, String)?#>,
-    //                                         weekVideoURLList: <#T##[(String, String)]#>,
-    //                                         weekReadingURLList: <#T##[(String, String)]#>,
-    //                                         supplementalMaterialsURLList: <#T##[(String, String)]#>,
-    //                                         practiceSheetsURLList: <#T##[(String, String)]#>,
-    //                                         guidedMeditationPlaceholder: <#T##[(String, String)]#>)
-    
+    static let week5 = WeekCourseContent(
+        lessonTitle: "Week 5 - Dealing with Difficult Emotions or Physical Pain",
+        lessonSubTitle: "Turning Toward...",
+        introTextParagraph1: ("Dealing with Difficult Emotions or Physical Pain", "So far in this course, when some difficulty presents itself during meditation in the form of a thought, emotion, or physical sensation, we have been simply acknowledging it and then “firmly but gently” returning to the object of awareness (for instance, breath in the sitting meditation). This week, we introduce a new way to deal with difficult emotions or physical pain, called #*\"Turning Toward\"*# that involves intentionally exploring the difficulty rather than moving away from it. This is a powerful method, but it is deeply counter-intuitive, because the last thing we think we want to do is to move closer to what's been troubling us."),
+        introTextParagraph2: ("", "When used as an informal practice, #*\"Turning Toward\"*# can be thought of as a more nuanced form of STOP (\"One-Minute Breathing Space\") that allows for a fuller exploration of what's happening. Although such an exploration might sound scary and uncomfortable, this is a gentle process, and is done in a special way, one that draws on the skills of awareness and non-judgment that you've been building so far in this course."),
+        introTextParagraph3: nil,
+        videosTextParagraph1: ("videos", "In *Turning Toward Difficulty* and *The Fist Exercise*, Vidyamala Burch describes this surprising and counter-intuitive way of dealing with difficult issues. In *One Moment at a Time*, she describes the moment her approach to dealing with her chronic pain changed and her life's work in understanding and teaching this approach began. The other videos, including *Pain x Resistance = Suffering*, deal with a variety of issues having to do with this week's topic, and in *The Three Components of Self-Compassion*, Kristin Neff talks about the importance of dealing with difficult emotional and physical issues with self-kindness and gentleness. If you are dealing with significant physical pain, be sure to watch Tara Brach's *Working Skillfully* with Pain and *Guided Reflection on Inhabiting this Body* in the supplementary section."),
+        videosTextParagraph2: nil,
+        videosTextParagraph3: nil,
+        readingsTextParagraph1: ("readings", "*Responding to Emotional or Physical Pain* describes how \"Turning Toward\" differs from more typical ways of dealing with difficulty. *\"Turning Toward\"* Difficult Emotions and *\"Turning Toward\" Physical Pain* are one-page descriptions that will be used for the informal practice this week. Vidyamala Burch's *Five-Step Model* inspired these informal practices and the corresponding \"Turning Toward\" meditations. Tara Brach's *Radical Acceptance* comes from her book by the same name and talks about the two \"wings\" of Radical Acceptance, clear seeing and self-compassion, both very important for \"Turning Toward\". #If you or a loved one is dealing with back pain, *The Strange Case of Chronic Back Pain*, in the Supplementary section, is a must-read.#"),
+        readingsTextParagraph2: nil,
+        readingsTextParagraph3: nil,
+        dailyPracticesTextParagraph1: ("daily practices", "For the *formal practice*, we introduce the *\"Turning Toward\" Meditation*. You will do this the first two days of this week, and on the following days you can choose from any of the practices you've learned so far. The \"Turning Toward\" meditation prepares you for doing the \"Turning Toward\" informal practice."),
+        dailyPracticesTextParagraph2: ("", "The *informal practice* will be to practice \"Turning Toward\" an unwanted emotion or physical pain that may have come up during the day. #Be sure to print and review *\"Turning Toward\" Difficult Emotions* and *\"Turning Toward\" Physical Pain* to use as guides for these informal practices.#"),
+        dailyPracticesTextParagraph3: nil,
+        miscellaneousTextParagraph1: ("", ""),
+        miscellaneousTextParagraph2: nil,
+        miscellaneousTextParagraph3: nil,
+        weekVideoURLList: [
+            ("*Turning Toward Difficulty* ~- Vidyamala Burch [12 min]]~", "https://www.youtube.com/watch?v=8aAATYSvNjc&index=1&list=PLbiVpU59JkVYtEL6-21TOfe3IELNMmOhO"),
+            ("*The Fist Exercise* ~- Vidyamala Burch [12 min]~", "https://www.youtube.com/watch?v=v6gFJRxgz5A&list=PLbiVpU59JkVYtEL6-21TOfe3IELNMmOhO&index=2"),
+            ("*One Moment at a Time* ~- Vidyamala Burch [2 min]~", "https://www.youtube.com/watch?v=Ez9s124sL8I&index=3&list=PLbiVpU59JkVYtEL6-21TOfe3IELNMmOhO"),
+            ("*The Gift and Power of Emotional Courage* ~- Susan David [16 min]~", "https://www.youtube.com/watch?v=NDQ1Mi5I4rg&list=PLbiVpU59JkVYtEL6-21TOfe3IELNMmOhO&index=4"),
+            ("*Mindfulness and Painful Emotions* ~- Steven Hayes (2 min)~", "https://www.youtube.com/watch?v=SHnULfYKXcI&index=5&list=PLbiVpU59JkVYtEL6-21TOfe3IELNMmOhO"),
+            ("*Pain x Resistance = Suffering * ~- Tara Brach (8 min)~", "https://www.youtube.com/watch?v=O8-exWpcngc&index=6&list=PLbiVpU59JkVYtEL6-21TOfe3IELNMmOhO"),
+            ("*The Three Components of Self-Compassion * ~- Kristin Neff [6 min]~", "https://www.youtube.com/watch?v=w3aba1ok5lE&list=PLbiVpU59JkVYtEL6-21TOfe3IELNMmOhO&index=8")
+        ],
+        weekReadingURLList: [
+            ("*Responding to Emotional or Physical Pain * ~- Dave Potter~", "https://palousemindfulness.com/docs/responding-to-difficulty.pdf"),
+            ("*\"Turning Toward\" Difficult Emotions* ~- guideline for Informal Practice~", "https://palousemindfulness.com/docs/turning-toward-emotional.pdf"),
+            ("*\"Turning Toward\" Physical Pain* ~- guideline for Informal Practice~", "https://palousemindfulness.com/docs/turning-toward-physical.pdf"),
+            ("*The Five-Step Model* ~- Vidyamala Burch~", "https://palousemindfulness.com/docs/vidyamala-5steps.pdf"),
+            ("*Radical Acceptance* ~- Tara Brach~", "https://palousemindfulness.com/docs/radical-acceptance.pdf")
+        ],
+        supplementalMaterialsURLList: [
+            ("*\"Inner Listening\" - An Introduction to Focusing* ~Time Magazine article~", "https://palousemindfulness.com/docs/science-of-anxiety.pdf"),
+            ("*Working Skillfully with Pain*  ~Time Magazine graphic~", "https://palousemindfulness.com/docs/anatomy-of-anxiety.pdf"),
+            ("*Guided Reflection on Inhabiting this Body* ~article by Bruce McEwen~", "https://aeon.co/essays/how-stress-works-in-the-human-body-to-make-or-break-us"),
+            ("*Lower Back Ache?* ~short video \"teaser\" by Robert Sapolsky [3 min]~", "https://www.youtube.com/watch?v=r8-jOAsvj2c&index=1&list=PLbiVpU59JkVZeQPQ1u5mS8U1c0V7J5OJU"),
+            ("*The Strange Case of Chronic Back Pain* ~book by Robert Sapolsky~", "http://www.amazon.com/Why-Zebras-Dont-Ulcers-Third/dp/0805073698"),
+            ("*Back Sense: Halting the Cycle of Chronic Back Pain* ~book by Susan Bauer-Wu~", "http://www.amazon.com/Leaves-Falling-Gently-Life-Limiting-Connectedness/dp/1572249994"),
+            ("*Working with Pain - Mindfulness Strategies* ~article by Jennifer Wolkin~", "http://www.mindful.org/mindful-approach-real-butterflies-stomach/"),
+            ("*How To Be Sick* ~article by Harriet Brown~", "https://palousemindfulness.com/docs/otherbrain.pdf"),
+            ("*Dealing with Panic Attacks* ~article by Harriet Brown~", "https://palousemindfulness.com/docs/otherbrain.pdf"),
+            ("*Mindfulness and Physical Pain* ~article by Harriet Brown~", "https://palousemindfulness.com/docs/otherbrain.pdf"),
+            ("*Buddhism's Pain Relief* ~article by Harriet Brown~", "https://palousemindfulness.com/docs/otherbrain.pdf"),
+            ("*Awakening Self-Compassion* ~article by Harriet Brown~", "https://palousemindfulness.com/docs/otherbrain.pdf"),
+            ("*Emotional Agility: Get Unstuck, Embrace Change* ~article by Harriet Brown~", "https://palousemindfulness.com/docs/otherbrain.pdf"),
+            ("*Mindfulness-Based Pain Management* ~article by Harriet Brown~", "https://palousemindfulness.com/docs/otherbrain.pdf"),
+            ("*Living Well With Pain & Illness* ~article by Harriet Brown~", "https://palousemindfulness.com/docs/otherbrain.pdf"),
+            ("*YOU Are Not Your Pain* ~article by Harriet Brown~", "https://palousemindfulness.com/docs/otherbrain.pdf"),
+            ("*Vidyamala Burch interview* ~article by Harriet Brown~", "https://palousemindfulness.com/docs/otherbrain.pdf"),
+            ("*Choosing to Live Well with Pain & Illness* ~article by Harriet Brown~", "https://palousemindfulness.com/docs/otherbrain.pdf"),
+            ("*Choosing to Live Well with Pain & Illness* ~Psychology Today~", "https://www.psychologytoday.com/blog/the-athletes-way/201405/how-does-the-vagus-nerve-convey-gut-instincts-the-brain")
+            
+        ],
+        practiceSheetsURLList: [
+            ("\(WeekHeadings.formalPracticeSheet.rawValue) - #\"Turning Toward\" Meditation#", "TBD"),
+            ("\(WeekHeadings.informalPracticeSheet.rawValue) - #\"Turning Toward\" informal practices#", "TBD")
+        ],
+        guidedMeditationPlaceholder: [
+            ("Meditation for Difficult Emotions (22 min)", "TBD"),
+            ("Meditation for Physical Pain (25 min)", "TBD")
+        ]
+    )
 }
