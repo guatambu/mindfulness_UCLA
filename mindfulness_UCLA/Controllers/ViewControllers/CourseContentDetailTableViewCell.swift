6542a06aa059bb6d91dd9a12bd7c83c6f15be2b2
michael guatambu davis <guatambu@dundak.com>
2019-10-22 23:10:46 -0700

    adds successful text search via NSRegularExpression to properly display text typeface formatting for all the various string inputs in CourseContentDetailTableViewCell.swift

--- mindfulness_UCLA/Controllers/ViewControllers/CourseContentDetailTableViewCell.swift
+++ mindfulness_UCLA/Controllers/ViewControllers/CourseContentDetailTableViewCell.swift
@@ -18,6 +18,7 @@ class CourseContentDetailTableViewCell: UITableViewCell {
     @IBOutlet weak var headingLabel: UILabel!
     @IBOutlet weak var bodyTextLabel: UILabel!
     
+    
     // array that holds the valid checked string tuples used to present the overview details
     var tupleStrings: (String, String)? {
         
@@ -64,35 +65,228 @@ class CourseContentDetailTableViewCell: UITableViewCell {
     }
     
     func applyStylesToRange(stringToFormat: String)  -> NSMutableAttributedString {
-        // 1
-        let fontDescriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: .body)
-        let boldFontDescriptor = fontDescriptor.withSymbolicTraits(.traitBold)
-        let boldFont = UIFont(descriptor: boldFontDescriptor!, size: 0)
-        let normalFont = UIFont.preferredFont(forTextStyle: .body)
         
-        // 2
-        let regexStr = "(\\*\\w+(\\s\\w+)*\\*)"
-        let regex = try! NSRegularExpression(pattern: regexStr)
-        let boldAttributes = [NSAttributedString.Key.font: boldFont]
+//        // create normal body font attribute
+//        let fontDescriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: .body)
+        let fontDescriptor = UIFontDescriptor.init(name: "GillSans", size: 16)
+        let normalFont = UIFont(descriptor: fontDescriptor, size: 16)
         let normalAttributes = [NSAttributedString.Key.font: normalFont]
         
-        // 3
-        let attributedString: NSMutableAttributedString = NSMutableAttributedString(string: stringToFormat)
+        // create bold typeface font attribute
+        let boldFontDescriptor = fontDescriptor.withSymbolicTraits(.traitBold)
+        let boldFont = UIFont(descriptor: boldFontDescriptor!, size: 16)
+        let boldAttributes = [NSAttributedString.Key.font: boldFont]
         
-        let searchRange = NSMakeRange(0, stringToFormat.count)
+        // create italic typeface font attribute
+        let italicFontDescriptor = fontDescriptor.withSymbolicTraits(UIFontDescriptor.SymbolicTraits.traitItalic)
+        let italicFont = UIFont(descriptor: italicFontDescriptor!, size: 16)
+        let italicAttributes = [NSAttributedString.Key.font: italicFont]
         
-        regex.enumerateMatches(in: stringToFormat, range: searchRange) {
-            match, flags, stop in
+        // create bold italics typeface font attributes
+        let boldItalicFontDescriptor = fontDescriptor.withSymbolicTraits(UIFontDescriptor.SymbolicTraits(arrayLiteral: .traitBold, .traitItalic))
+        let boldItalicFont = UIFont(descriptor: boldItalicFontDescriptor!, size: 16)
+        let boldItalicAttributes = [NSAttributedString.Key.font: boldItalicFont]
+        
+        // create small italics typeface font attributes
+        let smallItalicFontDescriptor = fontDescriptor.withSymbolicTraits(UIFontDescriptor.SymbolicTraits(arrayLiteral: .traitItalic))
+        let smallItalicFont = UIFont(descriptor: smallItalicFontDescriptor!, size: 12)
+        let smallItalicAttributes = [NSAttributedString.Key.font: smallItalicFont]
+        
+        // now pursue the string formatting search
+       let attributedString: NSMutableAttributedString = NSMutableAttributedString(string: stringToFormat)
+
+        let range = NSMakeRange(0, stringToFormat.utf16.count)
+        
+        var charactersRemovedFromString = 0
+        
+        
+        let boldRegex = try! NSRegularExpression(pattern: RegExPatterns.boldPattern.rawValue, options: NSRegularExpression.Options.caseInsensitive)
+        
+        let boldMatches = boldRegex.matches(in: stringToFormat, options: [], range: range)
+        
+        print("stringToFormat: \(stringToFormat)")
+        print("stringToFormat.count: \(stringToFormat.count)")
+        print("boldMatches: \(boldMatches)")
+        
+        var boldCounter = 0
+        
+        for match in boldMatches {
             
-            if let matchRange = match?.range(at: 1) {
-                attributedString.addAttributes(boldAttributes, range: matchRange)
-                // 4
-                let maxRange = matchRange.location + matchRange.length
-                if maxRange + 1 < attributedString.length {
-                    attributedString.addAttributes(normalAttributes, range: NSMakeRange(maxRange, 1))
-                }
-            }
+            print("i: \(boldCounter)")
+            print("charactersRemovedFromString: \(charactersRemovedFromString)")
+            
+//            if i == 0 {
+//
+//                charactersRemovedFromString = 0
+//            }
+            
+            let newRange = NSMakeRange(match.range.location - charactersRemovedFromString, match.range.length)
+            
+            attributedString.addAttributes(normalAttributes, range:newRange)
+            
+            let rangeOfFirstCharacter = NSMakeRange(match.range.location - charactersRemovedFromString, 1)
+            
+            attributedString.replaceCharacters(in: rangeOfFirstCharacter, with: "")
+            
+            charactersRemovedFromString += 2
+            boldCounter += 1
+            
+            let rangeOfLastCharacter = NSMakeRange(match.range.location + match.range.length - charactersRemovedFromString, 1)
+            
+            attributedString.replaceCharacters(in: rangeOfLastCharacter, with: "")
+            
         }
+        
+        let italicsRegex = try! NSRegularExpression(pattern: RegExPatterns.italicPattern.rawValue, options: NSRegularExpression.Options.caseInsensitive)
+        
+        let italicStringToFormat = attributedString.string
+        
+        let italicRange = NSMakeRange(0, italicStringToFormat.utf16.count)
+        
+        let italicMatches = italicsRegex.matches(in: italicStringToFormat, options: [], range: italicRange)
+        
+        var italicCounter = 0
+        
+        for match in italicMatches {
+            
+            if italicCounter == 0 {
+                
+                charactersRemovedFromString = 0
+            }
+            
+            print("n: \(italicCounter)")
+            print("charactersRemovedFromString: \(charactersRemovedFromString)")
+            
+            let newRange = NSMakeRange(match.range.location - charactersRemovedFromString, match.range.length)
+            
+            attributedString.addAttributes(italicAttributes, range:newRange)
+            
+            let rangeOfFirstCharacter = NSMakeRange(match.range.location - charactersRemovedFromString, 1)
+            
+            attributedString.replaceCharacters(in: rangeOfFirstCharacter, with: "")
+            
+            charactersRemovedFromString += 2
+            italicCounter += 1
+            
+            let rangeOfLastCharacter = NSMakeRange(match.range.location + match.range.length - charactersRemovedFromString, 1)
+            
+            attributedString.replaceCharacters(in: rangeOfLastCharacter, with: "")
+        }
+        
+        let smallItalicRegex = try! NSRegularExpression(pattern: RegExPatterns.smallItalicPattern.rawValue, options: NSRegularExpression.Options.caseInsensitive)
+        
+        let smallItalicStringToFormat = attributedString.string
+        
+        let smallItalicRange = NSMakeRange(0, smallItalicStringToFormat.utf16.count)
+        
+        let smallItalicMatches = smallItalicRegex.matches(in: smallItalicStringToFormat, options: [], range: smallItalicRange)
+        
+        var smallItalicCounter = 0
+        
+        for match in smallItalicMatches {
+            
+            if smallItalicCounter == 0 {
+                
+                charactersRemovedFromString = 0
+            }
+            
+            print("n: \(smallItalicCounter)")
+            print("charactersRemovedFromString: \(charactersRemovedFromString)")
+            
+            let newRange = NSMakeRange(match.range.location - charactersRemovedFromString, match.range.length)
+            
+            attributedString.addAttributes(smallItalicAttributes, range:newRange)
+            
+            let rangeOfFirstCharacter = NSMakeRange(match.range.location - charactersRemovedFromString, 1)
+            
+            attributedString.replaceCharacters(in: rangeOfFirstCharacter, with: "")
+            
+            charactersRemovedFromString += 2
+            smallItalicCounter += 1
+            
+            let rangeOfLastCharacter = NSMakeRange(match.range.location + match.range.length - charactersRemovedFromString, 1)
+            
+            attributedString.replaceCharacters(in: rangeOfLastCharacter, with: "")
+        }
+        
+        let boldItalicRegex = try! NSRegularExpression(pattern: RegExPatterns.boldItalicPattern.rawValue, options: NSRegularExpression.Options.caseInsensitive)
+        
+        let boldItalicStringToFormat = attributedString.string
+        
+        let boldItalicRange = NSMakeRange(0, boldItalicStringToFormat.utf16.count)
+        
+        let boldItalicMatches = boldItalicRegex.matches(in: boldItalicStringToFormat, options: [], range: boldItalicRange)
+        
+        var boldItalicCounter = 0
+        
+        for match in boldItalicMatches {
+            
+            if boldItalicCounter == 0 {
+                
+                charactersRemovedFromString = 0
+            }
+            
+            print("n: \(boldItalicCounter)")
+            print("charactersRemovedFromString: \(charactersRemovedFromString)")
+            
+            let newRange = NSMakeRange(match.range.location - charactersRemovedFromString, match.range.length)
+            
+            attributedString.addAttributes(boldItalicAttributes, range:newRange)
+            
+            let rangeOfFirstCharacter = NSMakeRange(match.range.location - charactersRemovedFromString, 1)
+            
+            attributedString.replaceCharacters(in: rangeOfFirstCharacter, with: "")
+            
+            charactersRemovedFromString += 2
+            boldItalicCounter += 1
+            
+            let rangeOfLastCharacter = NSMakeRange(match.range.location + match.range.length - charactersRemovedFromString, 1)
+            
+            attributedString.replaceCharacters(in: rangeOfLastCharacter, with: "")
+        }
+        
+        
+        print("attributedString: \(attributedString)")
+        
         return attributedString
     }
 }
+
+
+
+//        for (pattern, attributes) in replacements {
+//
+//            let regex = try! NSRegularExpression(pattern: pattern)
+//
+//            regex.enumerateMatches(in: stringToFormat, range: searchRange) {
+//                match, flags, stop in
+//
+//                if let matchRange = match?.range(at: 1) {
+//
+//                    print("Matched pattern: \(pattern)")
+//
+//                    let newRange = NSMakeRange(matchRange.location - numberOfCharactersRemovedFromStringToFormat, matchRange.length)
+//
+//                    attributedString.addAttributes(attributes, range: newRange)
+//
+////                    let maxRange = matchRange.location + matchRange.length
+////
+////                    if maxRange + 1 < attributedString.length {
+////                        attributedString.addAttributes(normalAttributes, range: NSMakeRange(maxRange, 1))
+////                    }
+//
+//                    let rangeOfFirstCharacterToRemove = NSRange(location: matchRange.location - numberOfCharactersRemovedFromStringToFormat, length: 1)
+//
+//                    attributedString.replaceCharacters(in: rangeOfFirstCharacterToRemove, with: "")
+//
+//
+//                    numberOfCharactersRemovedFromStringToFormat += 2
+//
+//                    let rangeOfLastCharacterToRemove = NSRange(location: matchRange.location + matchRange.length - numberOfCharactersRemovedFromStringToFormat, length: 1)
+//
+//                    attributedString.replaceCharacters(in: rangeOfLastCharacterToRemove, with: "")
+//                }
+//            }
+//        }
+
+
