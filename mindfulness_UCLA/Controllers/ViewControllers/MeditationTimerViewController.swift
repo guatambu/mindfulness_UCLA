f4501d5eb0efc961c998bb0c3c98204c63a734a6
michael guatambu davis <guatambu@dundak.com>
2019-10-30 19:35:40 -0700

    adds initial implementation of MeditationTimerVC display and timer functionality, still need to handle the startPauseButtonTapped funcitonality to fire and pause the timer

--- mindfulness_UCLA/Controllers/ViewControllers/MeditationTimerViewController.swift
+++ mindfulness_UCLA/Controllers/ViewControllers/MeditationTimerViewController.swift
@@ -7,70 +7,177 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MeditationTimerViewController: UIViewController {
     
     // MARK: Properties
+    
+    // UILabel(s) properties
     @IBOutlet weak var meditationTimerLabel: UILabel!
+    @IBOutlet weak var numericCountdownLabel: UILabel!
+    @IBOutlet weak var minutesLabel: UILabel!
+    // UIView(s) properties
     @IBOutlet weak var timerStackView: UIStackView!
     @IBOutlet weak var timerImageView: UIImageView!
     @IBOutlet weak var countdownView: UIView!
-    @IBOutlet weak var numericCountdownLabel: UILabel!
-    @IBOutlet weak var minutesLabel: UILabel!
-    @IBOutlet weak var minutesChoiceSlider: UISlider!
+    // UIButton(s) properties
+    @IBOutlet weak var startPauseButtonOutlet: UIButton!
     @IBOutlet weak var twoMinutesButtonOutlet: UIButton!
     @IBOutlet weak var fiveMinutesButtonOutlet: UIButton!
     @IBOutlet weak var tenMinutesButtonOutlet: UIButton!
     @IBOutlet weak var closeButtonOutlet: UIButton!
+    // UISlider property
+    @IBOutlet weak var minutesChoiceSlider: UISlider!
+    
+    var sliderTimer: Timer?
+    var seconds: TimeInterval = 0.00
+    
+    var audioPlayer = AVAudioPlayer()
     
     
     // MARK: ViewControllers Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupSlider()
+        setupPauseButton()
     }
     
     // MARK: Actions
     
+    @IBAction func startPauseButtonTapped(_ sender: UIButton) {
+        
+        startPauseButtonOutlet.setImage(UIImage(named: "mindfulness-pause-90"), for: UIControl.State.normal)
+        startPauseButtonOutlet.setTitle("", for: UIControl.State.normal)
+        
+        sliderTimer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(updateTimer)), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        
+        seconds = Double(sender.value) * 60
+        
+        numericCountdownLabel.text = setInitialTimerValue(time: seconds)
+        
+        startPauseButtonOutlet.isEnabled = true
+        startPauseButtonOutlet.setImage(UIImage(), for: UIControl.State.normal)
+        startPauseButtonOutlet.setTitle("start", for: UIControl.State.normal)
+    }
+    
+    
     @IBAction func twoMinutesButtonTapped(_ sender: UIButton) {
+        
+        seconds = 2 * 60
+        
+        numericCountdownLabel.text = setInitialTimerValue(time: seconds)
+        
+        startPauseButtonOutlet.isEnabled = true
+        startPauseButtonOutlet.setImage(UIImage(), for: UIControl.State.normal)
+        startPauseButtonOutlet.setTitle("start", for: UIControl.State.normal)
     }
     
     @IBAction func fiveMinutesButtonTapped(_ sender: UIButton) {
+        
+        seconds = 5 * 60
+        
+        numericCountdownLabel.text = setInitialTimerValue(time: seconds)
+        
+        startPauseButtonOutlet.isEnabled = true
+        startPauseButtonOutlet.setImage(UIImage(), for: UIControl.State.normal)
+        startPauseButtonOutlet.setTitle("start", for: UIControl.State.normal)
     }
     
     @IBAction func tenMinutesButtonTapped(_ sender: UIButton) {
+        
+        seconds = 10 * 60
+        
+        numericCountdownLabel.text = setInitialTimerValue(time: seconds)
+        
+        startPauseButtonOutlet.isEnabled = true
+        startPauseButtonOutlet.setImage(UIImage(), for: UIControl.State.normal)
+        startPauseButtonOutlet.setTitle("start", for: UIControl.State.normal)
     }
     
     @IBAction func closeButtonTapped(_ sender: UIButton) {
+        // dismiss the modally presented meditation timer viewcontroller
+        dismiss(animated: true, completion: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
-extension MeditationTimerViewController {
-    
-    // MARK: UISlider Setup
-    
-    
-}
 
 extension MeditationTimerViewController {
     
     // MARK: Helper Functions
     
-    func setTimer(countdown minutes: Int) {
+    func setupPauseButton() {
         
+        startPauseButtonOutlet.isEnabled = false
+        startPauseButtonOutlet.setImage(UIImage(), for: UIControl.State.normal)
+        startPauseButtonOutlet.setTitle("start", for: UIControl.State.normal)
+    }
+    
+    func setupSlider() {
+        
+        minutesChoiceSlider.minimumValue = 0.00
+        minutesChoiceSlider.maximumValue = 60.00
+        minutesChoiceSlider.setValue(0.00, animated: true)
+        
+    }
+    
+    // function to set the start timer value with round 10 second values
+    func setInitialTimerValue(time inSeconds: TimeInterval) -> String {
+        
+        var initialTimerValue = ""
+        
+        var seconds = 0
+        var minutes = 0
+        
+        minutes = Int(inSeconds) / 60
+        seconds =  (((Int(inSeconds) % 60) / 10) * 10)
+        
+        if seconds < 10 {
+            
+            initialTimerValue = "\(minutes):0\(seconds)"
+        } else {
+            
+            initialTimerValue = "\(minutes):\(seconds)"
+        }
+        
+        return initialTimerValue
+    }
+    
+    
+    // function to take in a track's given TimeInterval value in seconds and convert that to minutes and seconds
+    func convertSecondsToMinutes(time inSeconds: TimeInterval) -> String {
+        
+        var seconds = 0
+        var minutes = 0
+        
+        var duration = ""
+        
+        minutes = Int(inSeconds) / 60
+        seconds = Int(inSeconds) % 60
+        
+        if seconds < 10 {
+            
+            duration = "\(minutes):0\(seconds)"
+        } else {
+            
+            duration = "\(minutes):\(seconds)"
+        }
+        
+        return duration
+        
+    }
+    
+    // functipn to allow timer value to update the minutesLabel.text value
+    @objc func updateTimer() {
+        
+        seconds -= 1
+        
+        numericCountdownLabel.text = convertSecondsToMinutes(time: seconds)
     }
 }
