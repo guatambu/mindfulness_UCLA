17d6f542e9e7edc9fcbb5c633485b76ea003e211
michael guatambu davis <guatambu@dundak.com>
2019-11-09 21:07:14 -0800

    adds reset button for master count of GuidedMeditations to AdminCheckTableViewController with UIAlertController

--- mindfulness_UCLA/Controllers/ViewControllers/AdminCheckTableViewController.swift
+++ mindfulness_UCLA/Controllers/ViewControllers/AdminCheckTableViewController.swift
@@ -55,7 +55,8 @@ class AdminCheckTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        let resetItem = UIBarButtonItem(title: "reset", style: UIBarButtonItem.Style.plain, target: self, action: #selector(resetMasterCount))
+        navigationItem.rightBarButtonItem = resetItem
     }
 
     // MARK: - Table view data source
@@ -76,5 +77,26 @@ class AdminCheckTableViewController: UITableViewController {
         cell.detailTextLabel?.text = "\(Int(meditationCounts[indexPath.row]))"
 
         return cell
+    }
+    
+    
+    // MARK: - helper funtions
+    
+    // funciton that resets the guided meditations master count across all the various guided meditations.  a complete reset.
+    @objc func resetMasterCount() {
+        
+        let alert = UIAlertController(title: "Master Count Reset", message: "are you sure you want to reset the master count?", preferredStyle: UIAlertController.Style.actionSheet)
+        
+        let cancel = UIAlertAction(title: "cancel", style: UIAlertAction.Style.destructive, handler: nil)
+        
+        let pleaseReset = UIAlertAction(title: "please reset", style: UIAlertAction.Style.default) { (action) in
+            
+            GuidedMeditationsModelController.shared.update(count: GuidedMeditationsModelController.shared.counts[0], raisin: 0.0, bodyScan: 0.0, difficultEmotions: 0.0, lake: 0.0, lovingKindness: 0.0, mountain: 0.0, physicalPain: 0.0, r_a_i_n: 0.0, silent: 0.0, sitting: 0.0, sootheSoftenAllow: 0.0, yoga1: 0.0, yoga2: 0.0)
+        }
+        
+        alert.addAction(cancel)
+        alert.addAction(pleaseReset)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
